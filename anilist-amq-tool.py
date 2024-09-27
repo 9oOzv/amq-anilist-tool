4b466916e0db8f091aa3b149b82a40f390b418c8
Miika Piiroinen <miika.piiroinen@aalto.fi>
2024-09-28 02:15:49 +0300

    Add `isAdult` info to fetched media

--- anilist-amq-tool.py
+++ anilist-amq-tool.py
@@ -352,6 +352,7 @@ class Commands(object):
                     mediaListEntry {
                         id
                     }
+                    isAdult
                 }
             }
         }
@@ -484,6 +485,7 @@ class Commands(object):
                         romaji
                         english
                     }
+                    isAdult
                 }
             }
         }
